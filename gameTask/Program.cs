1b81e35b6b85a165897d6dbb2e759bc0f65018f2
rzamirzayev <rzatm@code.edu.az>
2022-12-14 17:07:57 +0400

    sent

--- gameTask/Program.cs
+++ gameTask/Program.cs
@@ -1,50 +1,21 @@
-﻿int bulletCount = 30;
-Console.WriteLine("Count of bullet:30");
-Console.WriteLine("Enter a value");
-bool isGameOn = true;
-static int shoot(ref int bulletCount)
-{
-    return bulletCount--;
+﻿Console.WriteLine("Please enter a sentence for reversing");
+string soz = Console.ReadLine();
+string soz2 = "";
+List<String> list = new List<String>();
 
-}
-static bool exitGame(ref bool isGameOn)
+foreach (var item in soz.Split(' '))
 {
-    isGameOn = !isGameOn;
-    return isGameOn;
+    list.Add(item);
 }
-static int reload(ref int bulletCount)
+foreach (var item in list)
 {
-    return bulletCount = 30;
-}
-static int noBullet(ref int bulletCount)
-{
-    return bulletCount = 0;
-}
-while (isGameOn)
-{
-    int answer = Convert.ToInt32(Console.ReadLine());
-    if (answer == 1)
+    for (int i = item.Length-1; i>=0 ; i--)
     {
-        shoot(ref bulletCount);
-        Console.WriteLine(bulletCount);
-        Console.WriteLine("Gulle atildi");
-    }else if (answer == 2)
-    {
-        noBullet(ref bulletCount);
-    }else if(answer == 3)
-    {
-        reload(ref bulletCount);
-    }else if (answer == 4)
-    {
-        Console.WriteLine("you left the game");
-        exitGame(ref isGameOn);
-    }else if(answer == 5)
-    {
-        Console.WriteLine(bulletCount);
+        soz2 += item[i];
+        if (i == 0)
+        {
+            soz2 += ' ';
+        }
     }
-    else
-    {
-        Console.WriteLine("Please enter one of these value: 1,2,3,4,5");
-    }
-
 }
+Console.WriteLine(soz2);
